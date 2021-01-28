b5e6a30c9c4b62c38909dfab273e0570b7ca3080
Jakub Dobrowolski <jd.kuba96@gmail.com>
2021-01-28 18:57:22 +0100

    Added basic Skeleton for AddLibraryDialog

--- src/gui/Add/AddLibraryDialog.cpp
+++ src/gui/Add/AddLibraryDialog.cpp
@@ -2,11 +2,45 @@
 
 AddLibraryDialog::AddLibraryDialog(QWidget* parent)
     : QDialog(parent, Qt::WindowTitleHint | Qt::WindowCloseButtonHint)
+    , m_mainLayout(new QVBoxLayout(this))
+    , m_libraryNameHLayout(new QHBoxLayout())
+    , m_libraryNameLabel(new QLabel(tr("Library Name")))
+    , m_libraryNameLineEdit(new QLineEdit())
+    , m_buttonsHLayout(new QHBoxLayout())
+    , m_addLibraryButton(new QPushButton(tr("Add")))
+    , m_cancelLibraryButton(new QPushButton(tr("Cancel")))
 {
-    
+    initGui();
 }
 
 AddLibraryDialog::~AddLibraryDialog()
 {
+    delete m_cancelLibraryButton;
+    delete m_addLibraryButton;
+    delete m_buttonsHLayout;
+    delete m_libraryNameLineEdit;
+    delete m_libraryNameLabel;
+    delete m_libraryNameHLayout;
+    delete m_mainLayout;
+}
 
+void AddLibraryDialog::initGui()
+{
+    resize(m_currentSize);
+    setMinimumSize(m_minimumSize);
+    setMaximumSize(m_maximumSize);
+
+    initLayouts();
+
+    m_libraryNameHLayout->addWidget(m_libraryNameLabel);
+    m_libraryNameHLayout->addWidget(m_libraryNameLineEdit);
+
+    m_buttonsHLayout->addWidget(m_addLibraryButton);
+    m_buttonsHLayout->addWidget(m_cancelLibraryButton);
+}
+
+void AddLibraryDialog::initLayouts()
+{
+    m_mainLayout->addLayout(m_libraryNameHLayout);
+    m_mainLayout->addLayout(m_buttonsHLayout);
 }
--- src/gui/Add/AddLibraryDialog.h
+++ src/gui/Add/AddLibraryDialog.h
@@ -2,6 +2,11 @@
 #define ADDLIBRARYDIALOG_H
 
 #include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
 
 class AddLibraryDialog : public QDialog
 {
@@ -10,6 +15,22 @@ public:
     explicit AddLibraryDialog(QWidget* parent = nullptr);
     ~AddLibraryDialog() override;
 private:
+    void initGui();
+    void initLayouts();
+
+    QVBoxLayout* m_mainLayout;
+
+    QHBoxLayout* m_libraryNameHLayout;
+    QLabel* m_libraryNameLabel;
+    QLineEdit* m_libraryNameLineEdit;
+
+    QHBoxLayout* m_buttonsHLayout;
+    QPushButton* m_addLibraryButton;
+    QPushButton* m_cancelLibraryButton;
+
+    QSize m_currentSize{ 450, 500 };
+    QSize m_minimumSize{ 400, 400 };
+    QSize m_maximumSize{ 500, 700 };
 };
 
 #endif //!ADDLIBRARYDIALOG_H
